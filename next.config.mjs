d9ddf697f497dc1e6932b890730047120a40e9d8
Shamimayesmin <shamimayesmin144@gmail.com>
2025-07-24 23:43:42 +0600

    fixing issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['server-minio-dev.labontest.tech'],
-      },
+	// images: {
+	//     domains: ['server-minio-dev.labontest.tech'],
+	//   },
+	images: {
+		unoptimized: true, // add this
+	},
 };
 
 export default nextConfig;
-
-
-
-  
